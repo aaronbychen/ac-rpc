76857d530d9498df7c2829e1befe8632991c342b
Aaron Chen <aaronbychen@gmail.com>
2025-03-09 02:52:36 +0800

    Update SpiLoader.java

--- yu-rpc-core/src/main/java/com/yupi/yurpc/spi/SpiLoader.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/spi/SpiLoader.java
@@ -15,12 +15,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * SPI 加载器
- * 自定义实现，支持键值对映射
+ * SPI loader
+ * Customized implement, support key-value mapping
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">程序员鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/aaronbychen">程序员鱼皮</a>
  */
 @Slf4j
 public class SpiLoader {
