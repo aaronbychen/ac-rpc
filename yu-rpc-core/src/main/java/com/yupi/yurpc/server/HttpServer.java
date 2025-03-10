1462217c9fe44709a04c5f671575e23cf7b16e18
Aaron Chen <aaronbychen@gmail.com>
2025-03-11 07:54:16 +0800

    Update HttpServer.java

--- yu-rpc-core/src/main/java/com/yupi/yurpc/server/HttpServer.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/server/HttpServer.java
@@ -1,16 +1,14 @@
 package com.yupi.yurpc.server;
 
 /**
- * HTTP 服务器接口
+ * HTTP server interface
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 public interface HttpServer {
 
     /**
-     * 启动服务器
+     * run server
      *
      * @param port
      */
