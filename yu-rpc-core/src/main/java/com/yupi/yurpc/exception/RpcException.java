a30fa9a49b16b9e74109857b24660d92e36d598b
Aaron Chen <aaronbychen@gmail.com>
2025-03-14 07:03:59 +0800

    Update RpcException.java

--- yu-rpc-core/src/main/java/com/yupi/yurpc/exception/RpcException.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/exception/RpcException.java
@@ -1,11 +1,9 @@
 package com.yupi.yurpc.exception;
 
 /**
- * 自定义异常类
+ * Customized exception class
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航学习圈</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 public class RpcException extends RuntimeException {
 
