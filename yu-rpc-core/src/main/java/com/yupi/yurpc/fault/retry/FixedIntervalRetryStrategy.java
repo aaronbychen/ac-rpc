9f1e8352722f8c717099ce09ee02dfa782142b9f
Aaron Chen <aaronbychen@gmail.com>
2025-03-17 07:59:08 +0800

    Update FixedIntervalRetryStrategy.java

--- yu-rpc-core/src/main/java/com/yupi/yurpc/fault/retry/FixedIntervalRetryStrategy.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/fault/retry/FixedIntervalRetryStrategy.java
@@ -9,17 +9,15 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 固定时间间隔 - 重试策略
+ * Fixed interval strategy - retry strategy
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航学习圈</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 @Slf4j
 public class FixedIntervalRetryStrategy implements RetryStrategy {
 
     /**
-     * 重试
+     * Retry
      *
      * @param callable
      * @return
