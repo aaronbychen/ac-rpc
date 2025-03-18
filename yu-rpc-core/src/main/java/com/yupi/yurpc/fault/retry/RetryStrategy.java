109a03d3b7bc546d17b0674ce52d458f21d3dee6
Aaron Chen <aaronbychen@gmail.com>
2025-03-18 19:11:22 +0800

    Update RetryStrategy.java

--- yu-rpc-core/src/main/java/com/yupi/yurpc/fault/retry/RetryStrategy.java
+++ yu-rpc-core/src/main/java/com/yupi/yurpc/fault/retry/RetryStrategy.java
@@ -5,16 +5,14 @@ import com.yupi.yurpc.model.RpcResponse;
 import java.util.concurrent.Callable;
 
 /**
- * 重试策略
+ * Retry strategy
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">鱼皮的编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航学习圈</a>
+ * @author <a href="https://github.com/aaronbychen">Aaron Chen</a>
  */
 public interface RetryStrategy {
 
     /**
-     * 重试
+     * Retry
      *
      * @param callable
      * @return
