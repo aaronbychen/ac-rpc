f3f0f57cd10af1d233a024fc26707bd15878bac0
Aaron Chen <aaronbychen@gmail.com>
2025-03-23 18:41:41 -0500

    Update file structures

---
+++ example-common/src/main/java/com/ac/example/common/service/UserService.java
@@ -0,0 +1,17 @@
+package main.java.com.ac.example.common.service;
+
+import main.java.com.ac.example.common.model.User;
+
+/**
+ * User service
+ */
+public interface UserService {
+
+    /**
+     * Get user
+     *
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+}
